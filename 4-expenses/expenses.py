b8401618919b96294889fe0f07f2e38af3db71f8
loordbarringtn <quantas@gmail.com>
2026-03-04 01:22:47 +0300

    fix

--- 4-expenses/expenses.py
+++ 4-expenses/expenses.py
@@ -5,5 +5,5 @@ exp_average = exp_sum / len(exp_list)
 exp_min = min(exp_list)
 exp_max = max(exp_list)
 
-exp_tup = (exp_max, exp_min, exp_average)
+exp_tup = (exp_min, exp_max, exp_sum)
 print(exp_tup)
